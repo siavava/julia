5a4be4678a2549bd65b6f9b561d0e8f7b4caf164
Amittai Siavava <amittaijoel@outlook.com>
2021-01-12 13:21:30 -0500

    update

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
+import math
+
 A = [1, 2, 3, 4]
 B = A
 B.append(7)
 A.append(9)
 print(A)
 print(B)
+
+def dosomestuff(var):
+    print(str(var))
+    
+dosomestuff(291)
+dosomestuff("what")
+dosomestuff(False)
+dosomestuff(math.pi)
